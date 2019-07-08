920d0a71ac2e68c148dc8821f7018803a26635ee
Sergey Zharko <Sergey_Zharko@epam.com>
2019-07-08 17:57:42 +0300

    description

--- sqlhana.js
+++ sqlhana.js
@@ -1,5 +1,7 @@
 /*
-Запуск: node sqlhana input output, где "input" - папка со скриптами mssql, "output" - папка для hana скриптов
+Запуск:
+    npm i
+    node sqlhana input output, где "input" - папка со скриптами mssql, "output" - папка для hana скриптов
 По умолчанию применяются значения: "sap-hana-ddl" и "src"
 */
 
